f5a461c49af5b442b520c3963663240b5fe70d86
mouse <linhongshimouse@gmail.com>
2010-11-06 21:14:55 +0800

    主页友情连接跳转功能添加，目前暂时为连接本页面

--- public/javascripts/muen.js
+++ public/javascripts/muen.js
@@ -24,3 +24,20 @@ strin = function(){
 		}
 	}
 	window.onload=strin;
+
+//主页友情链接跳转功能
+function select_change_to(){ 
+    var url = "";
+    var select_url = document.getElementById('selectd_fo').value;
+    if(select_url != "auction"){  
+        if(select_url == "shop")
+            url = "http://localhost:3000";
+        else if(select_url == "nick")
+            url = "http://localhost:3000";
+        else if(select_url == "info")
+            url = "http://localhost:3000";
+        else if(select_url == "web")
+            url = "http://localhost:3000";
+        window.open(url);
+    }
+}
